d6e134be9933da38aa11f37fc497e6ab11ed0de1
mudbungie <mudbungie@gmail.com>
2016-01-06 11:10:53 +0000

    major overhaul; Temp class handles most trivialities, not yet incorporated into thermostasis.py

---
+++ Temp.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+# Returns the C and F temperatures for configured 1wire themocouples
+from configobj import ConfigObj
+import os.path
+
+whereAmI = os.path.dirname(os.path.abspath(__file__)) + '/'
+config = ConfigObj(whereAmI + 'thermostasis.conf')
+
+Thermocouples = config['thermocouples']
+
+class Temp:
+    def __init__(self, serial, target=None, tolerance=None):
+        # Open a 1wire device, get the relevant data
+        path = '/sys/bus/w1/devices/' + serial + '/w1_slave'
+        with open(path, 'r') as thermFile:
+            tempLine = thermFile.read().splitlines()[1]
+            temp = int(tempLine.split('t=')[-1])
+        # Convert millidegrees C to usable numbers
+        self.CTemp = temp / 1000
+        self.FTemp = self.CTemp * 1.8 + 32
+        # Prep some soft strings for meatbags
+        self.CString = str(self.CTemp)[:7] + 'C'
+        self.FString = str(self.FTemp)[:7] + 'F'
+
+        # Check if everything is in acceptable range
+        if target and tolerance:
+            self.inTolerance = False
+            if self.CTemp > target + tolerance:
+                self.direction = 'too high'
+            elif self.CTemp < target - tolerance:
+                self.direction = 'too low'
+            else:
+                self.inTolerance = True
+        else:
+            self.inTolerance = True
+
+
+def getAllTemps():
+    temps = {}
+    for label, vals in Thermocouples.items():
+        if 'target' in vals and 'tolerance' in vals:
+            target = float(vals['target'])
+            tolerance = float(vals['tolerance'])
+            temp = Temp(vals['serial'], target, tolerance)
+        else:
+            temp = Temp(vals['serial'])
+        temps[label] = temp
+    return temps
+
+if __name__ == '__main__':
+    for label, temp in getAllTemps().items():
+        print(label + ' is at ' + temp.CString + 'C, and ' + 
+                                        temp.FString + 'F') 
--- makeSure.py
+++ makeSure.py
@@ -3,16 +3,15 @@
 import sendEmail
 import psutil
 import os
-import reportTemp
+import Temp
 
 # SMTP credentials
 def getAuth():
-    sourceLoc = os.path.dirname(os.path.abspath(__file__)) + '/'
-    with open(sourceLoc + 'auth', 'r') as file:
+    with open(Temp.config['alerts']['authFile'], 'r') as file:
         return file.read().splitlines()
 
 # Compose everything into the email and send it
-def alertMe(subject, body):
+def alertMe(subject, body=''):
     auth = getAuth()
     username = auth[0]
     password = auth[1]
@@ -20,22 +19,12 @@ def alertMe(subject, body):
 
     sendEmail.sendEmail(username, password, recipient, subject, body)
 
-def checkTemp(target, tolerance, scale='C'):
-    thermostats = reportTemp.reportAllTemps()
-
-    inTolerance = False
-
-    for label, temps in thermostats.items():
-        if temps[scale] > target + tolerance:
-            direction = 'too high'
-        elif temps[scale] < target - tolerance:
-            direction = 'too low'
-        else:
-            inTolerance = True
-            direction = 'in tolerance'
-        if not inTolerance:
-            alertMe(label + ' temp ' + direction + ': ' + str(temps['C'])[:7] + 'C ' + 
-                                    str(temps['F'])[:7] + 'F', '')
+def checkTemps():
+    temps = Temp.getAllTemps()
+    for label, temp in Temp.getAllTemps().items():
+        if temp.inTolerance == False:
+            alertMe(label + ' ' + temp.direction + ': ' + 
+                        temp.CString + ' ' + temp.FString)
 
 def isSystemRunning(cmdString):
     theSystemIsRunning = False
@@ -46,16 +35,13 @@ def isSystemRunning(cmdString):
 
     return theSystemIsRunning
 
-def checkThings(cmdString, target, tolerance):
+def checkThings(cmdString):
     systemOnline = isSystemRunning(cmdString)
     if systemOnline == False:
-        alertMe("the system isn't running", '')
-    checkTemp(target, tolerance)
+        alertMe('the system isn\'t running')
+    checkTemps()
 
 if __name__ == '__main__': 
     cmdString = 'python3 /home/pi/thermostasis/thermostasis.py'
-    # units in millidegress C
-    target = 23.889
-    tolerance = 2
-
-    checkThings(cmdString, target, tolerance)
+    isSystemRunning(cmdString)
+    checkTemps()
